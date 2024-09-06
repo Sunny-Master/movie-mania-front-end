b554a623eb1186cbafa712c3e516d849779fa2e0
AlexG <Alexanderg.savitr@gmail.com>
2024-09-06 18:45:21 -0500

    Add bar components for movie concept and celeb

---
+++ src/components/MovieBar/MovieBar.jsx
@@ -0,0 +1,7 @@
+const MovieBar = () => {
+  return ( 
+    <h1>Movie Bar</h1>
+  )
+}
+
+export default MovieBar;
---
+++ src/components/MovieConBar/MovieConBar.jsx
@@ -0,0 +1,7 @@
+const MovieConBar = () => {
+  return (  
+    <h1>Movie Concept Bar</h1>
+  )
+}
+
+export default MovieConBar;
