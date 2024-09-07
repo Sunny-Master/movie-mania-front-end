6351ffd3d734356b3bc170e73bb37e7fa0754e6d
AlexG <Alexanderg.savitr@gmail.com>
2024-09-07 14:02:50 -0500

    Add movieConCard

--- src/components/AuthorInfo/AuthorInfo.jsx
+++ src/components/AuthorInfo/AuthorInfo.jsx
@@ -1,14 +1,18 @@
 // components
 import DateCard from '../DateCard/DateCard'
 
+// assets
+import profileIcon from '../../assets/icons/profile.png'
+
 // css
 import styles from './AuthorInfo.module.css'
 
 const AuthorInfo = ({ content }) => {
+  const photo = content.author.photo ? content.author.photo : profileIcon
 
   return (
     <div className={styles.container}>
-      <img src={content.author.photo} alt="The user's avatar" />
+      <img src={photo} alt="The user's avatar" />
       <section>
         <h4>{content.author.name}</h4>
         <DateCard createdAt={content.createdAt} />
