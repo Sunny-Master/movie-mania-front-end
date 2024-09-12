51936f327269781e871868895830a93b4095193e
AlexG <Alexanderg.savitr@gmail.com>
2024-09-11 21:09:23 -0500

    Add final styling

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -12,7 +12,16 @@ const Landing = ({ user }) => {
 
   return (
     <main className={styles.container}>
-      <h1>hello, {user.name}</h1>
+      <h1>Hello, {user.name}!</h1>
+      <h2>Welcome to Movie Mania</h2>
+      <div>
+        <h1>Whats the point?</h1>
+        <p>Movie Mania is all about</p>
+      </div>
+      <div>
+        <h1>About us!</h1>
+        <p>This is that info</p>
+      </div>
     </main>
   )
 }
