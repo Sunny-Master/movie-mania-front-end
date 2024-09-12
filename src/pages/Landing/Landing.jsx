1bafa28563c1881900c957370b43dc8422731c8e
AlexG <Alexanderg.savitr@gmail.com>
2024-09-11 21:50:37 -0500

    Style landing

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -12,15 +12,10 @@ const Landing = ({ user }) => {
 
   return (
     <main className={styles.container}>
-      <h1>Hello, {user.name}!</h1>
-      <h2>Welcome to Movie Mania</h2>
+      <h1>Hello, {user.name}! Welcome to Movie Mania</h1>
       <div>
-        <h1>Whats the point?</h1>
+        <h1>What is Movie Mania</h1>
         <p>Movie Mania is all about</p>
-      </div>
-      <div>
-        <h1>About us!</h1>
-        <p>This is that info</p>
       </div>
     </main>
   )
